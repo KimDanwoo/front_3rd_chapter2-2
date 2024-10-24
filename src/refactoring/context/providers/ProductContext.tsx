50a00eb0ccc12fd1a607beb92066014276897264
KimDanwoo <dudgh9198@naver.com>
2024-10-24 15:45:32 +0900

    refactor: context 재 작성

--- src/refactoring/context/providers/ProductContext.tsx
+++ src/refactoring/context/providers/ProductContext.tsx
@@ -1,10 +1,20 @@
 import { createContext, ReactNode } from 'react'
 import { Product } from '@/types'
-import { useContextProvider, useDiscount, useProduct, ProductHook, DiscountHook } from '@/refactoring/hooks'
+import {
+  useContextProvider,
+  useDiscount,
+  useProduct,
+  ProductHook,
+  DiscountHook,
+  NewProductHook,
+  ProductEditorHook,
+  useProductEditor,
+  useNewProduct,
+} from '@/refactoring/hooks'
 
 const ERROR_MESSAGE = 'useProductContext는 ProductProvider 내부에서만 사용할 수 있습니다.'
 
-interface ProductContextType extends ProductHook, DiscountHook {}
+interface ProductContextType extends ProductHook, NewProductHook, ProductEditorHook, DiscountHook {}
 
 interface ProductProviderProps {
   children: ReactNode
@@ -13,18 +23,21 @@ interface ProductProviderProps {
 const ProductContext = createContext<ProductContextType | null>(null)
 
 export const ProductProvider = ({ children, initialProducts = [] }: ProductProviderProps) => {
-  const { ...productProps } = useProduct(initialProducts)
-
-  const { ...discountProps } = useDiscount({
-    products: productProps.products,
-    updateProduct: productProps.updateProduct,
-    editingProduct: productProps.editingProduct,
-    setEditingProduct: productProps.setEditingProduct,
+  const product = useProduct(initialProducts)
+  const productEditor = useProductEditor(product.updateProduct)
+  const newProduct = useNewProduct(product.addProduct)
+  const discount = useDiscount({
+    products: product.products,
+    updateProduct: product.updateProduct,
+    editingProduct: productEditor.editingProduct,
+    setEditingProduct: productEditor.setEditingProduct,
   })
 
   const contextValue = {
-    ...productProps,
-    ...discountProps,
+    ...product,
+    ...productEditor,
+    ...newProduct,
+    ...discount,
   }
 
   return <ProductContext.Provider value={contextValue}>{children}</ProductContext.Provider>
